e0974ab3e54c55b86921334f47ae2640748f84e3
Curtis Pollard <clp.the.second@gmail.com>
2022-10-19 15:35:44 -0700

    Hey now

--- app.js
+++ app.js
@@ -17,18 +17,34 @@ const menuItems = [{
     price: 2
 }]
 
+const cone = [{
+    name: 'Suger Cone',
+    image: '',
+    price: 1
+}, {
+    name: 'Bowl',
+    image: '',
+    price: 1
+}, {
+    name: 'Waffle Cone',
+    image: 'https://freepngimg.com/thumb/ice_cream/24787-5-ice-cream-cone-hd.png',
+    price: 2
+},
+]
+
 let orders = []
 
 function drawMenu() {
     let flavorTemplate = ''
     menuItems.forEach(flavor => {
         flavorTemplate += `
-        <div class="background2 col-6 p-3 iceCreamFlavor" onclick="order('${flavor.name}')">
-            <p class="bg-info fs-5"><b>${flavor.name}</b></p>               
-            <img class=" mt-0 img-fluid" src="${flavor.image}">
-            <p class="bg-info fs-5">$ ${flavor.price}</p>
+        <div class="col-5 text-center m-2 bg-pink row p-1 iceCreamFlavor rounded" onclick="order('${flavor.name}')">
+            <p class="bg-medium shadow-lg p-1 pe-0 mb-1 rounded fs-3 title-font">${flavor.name} <span class="fs-5 ps-3">$${flavor.price}</span></p>
+            <div class="">
+                <img class=" mt-0 img-fluid" src="${flavor.image}">
+            </div>
         </div>
-    `})
+    `}) 
     console.log(flavorTemplate);
     let menuElm = document.getElementById('menu-items')
     menuElm.innerHTML = flavorTemplate
