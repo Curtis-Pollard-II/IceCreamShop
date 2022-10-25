4764da963de437a1b5cc27cf1507944c921098dc
Curtis Pollard <clp.the.second@gmail.com>
2022-10-25 16:54:45 -0700

    spscc 3

--- app.js
+++ app.js
@@ -1,58 +1,50 @@
 const menuItems = [{
+        isCone: true,
+        name: 'Cake',
+        image: 'https://mattcones.com/wp-content/uploads/2017/03/large-cake-cone.png',
+        price: 1
+    }, {
+        isCone: true,
+        name: 'Bowl',
+        image: 'https://www.nicepng.com/png/full/361-3610970_12-oz-compostable-sugarcane-bagasse-round-bowls-disposable.png',
+        price: 1
+    }, {
+        isCone: true,
+        name: 'Waffle',
+        image: 'https://freepngimg.com/thumb/ice_cream/24787-5-ice-cream-cone-hd.png',
+        price: 2
+    },{
+    isCone: false,
     name: 'Cookie Dough',
     image: 'https://www.hersheyicecream.com/products/images/premium-gold/cookie-doughlicious-scoop2.png',
     price: 3
 }, {
+    isCone: false,
     name: 'Vanilla',
     image: 'https://www.hersheyicecream.com/products/images/premium-gold/old-fashioned-vanilla-scoop2.png',
     price: 2
 }, {
+    isCone: false,
     name: 'Strawberry',
     image: 'https://www.hersheyicecream.com/products/images/lighter-side/rfnsa-strawberry-scoop2.png',
     price: 2
-},
-{
+}, {
+    isCone: false,
     name: 'Chocolate',
     image: 'https://www.hersheyicecream.com/products/images/premium/heavenly-hazelnut-scoop2.png',
     price: 2
-}]
-
-const cone = [{
-    name: 'Cake',
-    image: 'https://mattcones.com/wp-content/uploads/2017/03/large-cake-cone.png',
-}, {
-    name: 'Bowl',
-    image: 'https://www.nicepng.com/png/full/361-3610970_12-oz-compostable-sugarcane-bagasse-round-bowls-disposable.png',
-}, {
-    name: 'Waffle',
-    image: 'https://freepngimg.com/thumb/ice_cream/24787-5-ice-cream-cone-hd.png',
 },
 ]
 
 let orders = []
-let orderCones = []
 
-function drawCone() {
-    let coneTemplate = ''
-    cone.forEach(coneType => {
-        coneTemplate += `
-        <div class="col-10 text-center m-2 bg-medium row p-1 rounded" onclick="order('${coneType.name}')">
-            <p class="bg-pink shadow-lg p-1 pe-0 mb-1 rounded fs-3 title-font">${coneType.name}</p>
-            <div class="">
-                <img class=" mt-0 img-fluid" src="${coneType.image}">
-            </div>
-        </div>
-    `}) 
-    let menuElm = document.getElementById('cone-type')
-    menuElm.innerHTML = coneTemplate
-}
-drawCone()
+
 
 function drawMenu() {
     let flavorTemplate = ''
     menuItems.forEach(flavor => {
         flavorTemplate += `
-        <div class="col-5 text-center m-2 bg-pink row p-1 rounded" onclick="order('${flavor.name}')">
+        <div class="col-3 text-center m-2 p-1 bg-pink rounded" onclick="order('${flavor.name}')">
             <p class="bg-medium shadow-lg p-1 pe-0 mb-1 rounded fs-3 title-font">${flavor.name} <span class="fs-5 ps-3">$${flavor.price}</span></p>
             <div class="">
                 <img class=" mt-0 img-fluid" src="${flavor.image}">
@@ -63,8 +55,6 @@ function drawMenu() {
     menuElm.innerHTML = flavorTemplate
 }
 drawMenu()
-
-
 
 
 function drawCart() {
@@ -81,14 +71,11 @@ drawCart()
 
 function order(selectYourFlavor) {
     let foundFlavor = menuItems.find(fla => fla.name == selectYourFlavor)
-    let foundCone = cone.find(c => c.name == selectYourFlavor)
-    orders.unshift(foundFlavor, foundCone)
+    orders.unshift(foundFlavor)
 
     drawCart()
     drawTotal()
 }
-
-
 
 
 function drawTotal() {
